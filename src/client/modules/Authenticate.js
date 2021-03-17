7bfd2ea5f91c2bb549dd012ce10793791c9f7f61
Ed Armstrong <ed@frar.ca>
2021-03-17 10:21:43 -0400

    working copy, pre merge with other work

--- src/client/modules/Authenticate.js
+++ src/client/modules/Authenticate.js
@@ -7,29 +7,17 @@ class Authenticate {
 
     loadClient() {
         return new Promise((resolve, reject) => {
-            gapi.load('client', () => this.__initClient(resolve, reject));
+            gapi.load('client:auth2', () => this.__initClient(resolve, reject));
         });
-        // if (gapi.auth2){
-        //     return new Promise((resolve, reject) => {
-        //         gapi.load('client', () => this.__initClient(resolve, reject));
-        //     });
-        // } else {
-        //     return new Promise((resolve, reject) => {
-        //         gapi.load('client:auth2', () => this.__initClient(resolve, reject));
-        //     });
-        // }
     }
 
     __initClient(resolve, reject) {
-        console.log("initializing auth 2");
         gapi.client.init({
             apiKey: this.developerKey,
             clientId: this.clientId,
             discoveryDocs: this.discoveryDocs,
             scope: this.scope
         }).then(function (result) {
-            console.log("RESOLVE INIT");
-            console.log(result);
             resolve();
         }, function(error) {
             console.log("ERROR INIT");
