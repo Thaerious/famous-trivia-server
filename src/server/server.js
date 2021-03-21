0812538bddba46389ff82ef64d212d6852b7c3d9
Ed Armstrong <ed@frar.ca>
2021-03-21 14:17:09 -0400

    cleaned up sever in preparation for launcher

--- src/server/server.js
+++ src/server/server.js
@@ -4,8 +4,6 @@ import helmet from 'helmet';
 import cookieParser from "cookie-parser";
 import UserAgent from 'express-useragent';
 
-// import {urlGoogle, getGoogleAccountFromCode} from './google-util.js';
-
 const port = 8000;
 const app = Express();
 const server = http.createServer(app);
@@ -13,7 +11,7 @@ const server = http.createServer(app);
 app.use(helmet()); // automatic security settings
 app.use(UserAgent.express()); // use to determine what the connection is using (phone,browser etc)
 
-app.use("*.html", function (req, res, next) {
+function catchDesignPage(req, res, next) {
     let csp = extractCSP(res);
     csp["default-src"] = "'self' *.google.com *.googleapis.com 'unsafe-inline'";
     csp["style-src"] = "'self' *.google.com 'unsafe-inline'";
@@ -21,8 +19,13 @@ app.use("*.html", function (req, res, next) {
     delete csp["script-src-attr"];
     res.set("Content-Security-Policy", concatCSP(res, csp));
     next();
-});
+}
 
+/**
+ * Extract Content Security Policy from header
+ * @param res
+ * @returns {{}}
+ */
 function extractCSP(res){
     let csp = res.get("Content-Security-Policy").split(";");
     let dict = {};
@@ -35,6 +38,13 @@ function extractCSP(res){
     return dict;
 }
 
+/**
+ * Prepare Content Security Policy for header.
+ * @param res
+ * @param csp
+ * @returns {string}
+ */
+
 function concatCSP(res, csp){
     let policyString = "";
     for (let key in csp){
@@ -43,17 +53,9 @@ function concatCSP(res, csp){
     return policyString;
 }
 
+app.use("/home.html", catchDesignPage);
+app.use("/editor.html", catchDesignPage);
 app.use(Express.static('public'));
-
-app.get("/google-url", function (req, res, next) {
-    // res.send(urlGoogle());
-});
-
-app.get("/google-cb", async function (req, res, next) {
-    let googleConsent = await getGoogleAccountFromCode(req.query.code);
-    res.redirect('/pages/join-success.html');
-    res.end();
-});
 
 server.listen(port, () => {console.log(`HTTP listener started on port ${port}`)});
 
