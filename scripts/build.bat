67bcf0fbeb7a76603cc5ffcb962e3adf06a054c2
Ed Armstrong <ed@frar.ca>
2021-05-26 19:36:55 -0400

    adding service file

--- scripts/build.bat
+++
@@ -1,3 +0,0 @@
-call sass src/styles/editor.scss:public/styles/generated/editor.css
-call sass src/styles/game_board.scss:public/styles/generated/game_board.css
-call sass src/styles/multiple_choice_pane.scss:public/styles/generated/multiple_choice_pane.css
