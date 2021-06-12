75930f1e065cd877b5c9aae2931bbc3cf0253cb2
Ed Armstrong <ed@frar.ca>
2021-06-12 12:58:21 -0400

    update systemd install file

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp cp accessory/trivia.service /etc/systemd/system
-sed -i "s/<USER>/"$USER"/g" /etc/systemd/system/trivia.service
+cp accessory/trivia.service /etc/systemd/system
+sed -i "s/<USER>/"$1"/g" /etc/systemd/system/trivia.service
 sed -i "s#<WD>#"$(pwd)"#g" /etc/systemd/system/trivia.service
 sed -i "s#<NODE>#"$(which node)"#g" /etc/systemd/system/trivia.service
